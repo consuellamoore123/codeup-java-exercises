ac17fcd23771842667b5b686e89ecfb043440b3d
Consuella Moore <consuellamoore123@gmail.com>
2023-05-15 13:59:50 -0500

    completed exercise

---
+++ src/ConsoleExercises.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class ConsoleExercises {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double pi = 3.14159;
+        System.out.printf("The value of %s is approximately 3.14.", pi);
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("type three words ");
+        String userInput = scanner.nextLine();
+
+        System.out.println("You entered: --> \"" + userInput + "\" <--");
+
+
+
+
+    }
+    }
--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,53 @@
+import org.w3c.dom.ls.LSOutput;
+
+import java.sql.SQLOutput;
+
 public class HelloWorld {
     public static void main (String[] args) {
      System.out.println("HelloWorld");
+
+//        int myFavoriteNumber = 7;
+//        System.out.println(myFavoriteNumber);
+//
+//       String myString = "hello";
+//        System.out.println(myString);
+//
+//        long yourNumber = 123123134111L;
+//        System.out.println(yourNumber);
+//
+//       float myNumber = 3.14f;
+//        System.out.println(myNumber);
+//
+//        double theNumber = 3.14;
+//        System.out.println(theNumber);
+//
+////        int x = 5;
+////        System.out.println(x++);
+////        System.out.println(x);
+//
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+//
+//        int three = "three";
+
+        int x = 123;
+        System.out.println("my number is  " +x);
+
+        char symbol = '@';
+        System.out.println(symbol);
+
+
+
+
+
     }
+
+
+
+
 }
