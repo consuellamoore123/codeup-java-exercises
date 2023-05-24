b32ada347c89f29b2d8dd467f18b549266b22089
Consuella Moore <consuellamoore123@gmail.com>
2023-05-24 18:49:08 -0500

    updated

--- src/util/Input.java
+++ src/util/Input.java
@@ -62,5 +62,12 @@ public class Input {
         return getDouble(min, max,prompt);
     }
 
-
+    public static void main(String[] args) {
+        Input inputOne = new Input();
+//        System.out.println("Get String: ");
+//        System.out.println(inputOne.getString());
+//        System.out.println("yesNo: ");
+//        System.out.println(inputOne.yesNo());
+        System.out.println(inputOne.getInt(1, 10,"give me an integer between 1-10"));
+    }
 }
